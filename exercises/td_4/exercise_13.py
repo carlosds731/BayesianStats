92c5ab014ec11b7c7ee71af25b92584c79998b06
carlosds731 <carlosfernandezds730@gmail.com>
2021-01-27 17:19:05 +0100

    sample code on exercise 13

--- exercises/td_4/exercise_13.py
+++ exercises/td_4/exercise_13.py
@@ -10,8 +10,7 @@ from scipy import stats
 def get_posterior_part_a(data, a, b, number_of_samples):
     with pm.Model() as our_model:
         theta = pm.Uniform('theta', lower=a, upper=b)
-        
-        y = pm.Poisson('y', mu=1/theta,
+        y = pm.Poisson('y', mu=1 / theta,
                        observed=data)
 
         return pm.sample(number_of_samples, progressbar=True)
@@ -27,3 +26,10 @@ def get_posterior_part_b(data, l, h, sigma_0, number_of_samples):
                       observed=data)
 
         return pm.sample(number_of_samples, progressbar=True)
+
+
+if __name__ == "__main__":
+    data = stats.poisson.rvs(mu=10, size=100)
+    trace = get_posterior_part_a(data, 0, 1, 1000)
+    az.plot_posterior(trace)
+    plt.show()
